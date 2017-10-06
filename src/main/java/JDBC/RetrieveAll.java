9326c8a08c638db8599b04136515bdfb85bf9093
nicks189 <nsteger189@gmail.com>
2017-10-06 13:50:39 -0500

    JDBC testing

---
+++ src/main/java/JDBC/RetrieveAll.java
@@ -0,0 +1,32 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class RetrieveAll {
+
+    public static void main(String[] args) throws SQLException {
+
+        try (
+                Connection con = DBUtil.getConnection(DBType.MYSQL);
+                PreparedStatement pst = con.prepareStatement("SELECT * FROM notes");
+                ResultSet rs = pst.executeQuery();
+                ) {
+
+            while (rs.next()) {
+                System.out.print(rs.getInt(1));
+                System.out.print(": ");
+                System.out.print(rs.getString(2));
+                System.out.print(" ");
+                System.out.println(rs.getString(3));
+            }
+
+        } catch (SQLException ex) {
+
+            DBUtil.processException(ex);
+
+        }
+    }
+}
