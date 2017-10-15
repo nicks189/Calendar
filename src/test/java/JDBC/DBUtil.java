8574e7956675a4f1758a45e9bc7f5f0387a140d9
nicks189 <nsteger189@gmail.com>
2017-10-15 11:28:06 -0500

    update tests

---
+++ src/test/java/JDBC/DBUtil.java
@@ -0,0 +1,61 @@
+package JDBC;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@SuppressWarnings("Duplicates")
+public class DBUtil {
+    public static Connection getConnection(DBType type) throws SQLException {
+        Properties props = new Properties();
+        FileInputStream in = null;
+
+        try {
+            switch (type) {
+                // Currently only support MySQL
+                case MYSQL:
+                    in = new FileInputStream("src/main/resources/db.properties");
+                    props.load(in);
+                    String url = props.getProperty("db.url");
+                    String user = props.getProperty("db.user");
+                    String passwd = props.getProperty("db.passwd");
+                    return DriverManager.getConnection(url, user, passwd);
+            }
+        } catch (FileNotFoundException ex) {
+
+            Logger lgr = Logger.getLogger(RetrieveAll.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        } catch (IOException ex) {
+
+            Logger lgr = Logger.getLogger(RetrieveAll.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        } finally {
+
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+                Logger lgr = Logger.getLogger(RetrieveAll.class.getName());
+                lgr.log(Level.SEVERE, ex.getMessage(), ex);
+            }
+        }
+        System.exit(-1);
+        // Redundant return stmt
+        return null;
+    }
+
+    public static void processException(SQLException e) {
+        Logger lgr = Logger.getLogger(RetrieveAll.class.getName());
+        lgr.log(Level.SEVERE, e.getMessage(), e);
+        lgr.log(Level.SEVERE, e.getSQLState(), e);
+    }
+}
---
+++ src/test/java/JDBC/Version.java
@@ -0,0 +1,59 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Version {
+
+    public static void main(String[] args) {
+
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+
+            con = DriverManager.getConnection("jdbc:mysql://localhost/calendar_db?" +
+                                                  "user=cal_user&password=3blue8");
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT VERSION()");
+
+            if (rs.next()) {
+
+                System.out.println(rs.getString(1));
+            }
+
+        } catch (SQLException ex) {
+
+            Logger lgr = Logger.getLogger(Version.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        } finally {
+
+            try {
+
+                if (rs != null) {
+                    rs.close();
+                }
+
+                if (st != null) {
+                    st.close();
+                }
+
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+
+                Logger lgr = Logger.getLogger(Version.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
+    }
+}
